b66b318885236730069c926e9579fa04eef9edb6
Carlos-SSH <carlos.anezeh@me.com>
2024-03-09 10:40:30 -0600

    Fix directory name

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import { HomePage } from './home/HomePage'
+import styled from '@emotion/styled'
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6rem;
+  min-height: 100vh;
+`
+
 
 export default function Home() {
   return (
-    <main>
+    <Main>
       <HomePage />
-    </main>
+    </Main>
   );
 }
